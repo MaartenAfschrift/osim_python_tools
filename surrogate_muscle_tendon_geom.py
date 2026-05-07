daa3615d213dd8f116cd25bdba981d778d1f48c2
Afschrift <m.a.afschrift@vu.nl>
2026-05-07 15:56:54 +0200

    init muscle polynomials

---
+++ surrogate_muscle_tendon_geom.py
@@ -0,0 +1,26 @@
+# methods to approximate muscle-tendon geometry using surrogate models
+# --------------------------------------------------------------
+
+# functions to add
+# create dummy motion (data to fit model)
+# function to fit surrogate model to data
+# functions to fit polynomial
+
+
+def create_dummy_motion(coordinates, n_datapoints, lower_bound, upper_bound, coordinate_defaults):
+    # input arguments
+    # coordinates: list of coordinate names (e.g., ['hip_flexion', 'knee_angle'])
+    # n_datapoints: number of datapoints
+    # lower_bound: lower bound coordinates
+    # upper_bound: upper bound coordinates
+    # coordinate_defaults: default value for all coordinates in the model
+
+
+
+
+
+
+
+
+
+
